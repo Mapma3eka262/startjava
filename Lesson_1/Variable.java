c28d9ff807f72c257886f17a5f9e2b6d1812685b
Mapma3eka262 <vlaskofanov@gmail.com>
2020-09-28 21:17:16 +0500

    Изменения согласно комментарию в Slack

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,21 +1,21 @@
 public class Variable {
-	public static void main(String[] args) {
-  	byte core = 6;
-  	short ram = 12;
-  	int ssd = 120;
-  	long hdd = 1024;
-  	float cpuSpeed = 3.8f;
-  	double assemblyOS = 19041.508;
-  	char cpu = 'A';
-  	boolean windows = true;
+  public static void main(String[] args) {
+    byte core = 6;
+    short ram = 12;
+    int ssd = 120;
+    long hdd = 1024;
+    float cpuSpeed = 3.8f;
+    double assemblyOS = 19041.508;
+    char cpu = 'A';
+    boolean windows = true;
 
-  	System.out.println("core " + core);
-  	System.out.println("RAM " + ram);
-  	System.out.println("SSD " + ssd);
-  	System.out.println("HDD " + hdd);
-  	System.out.println("CPU Speed " + cpuSpeed);
-  	System.out.println("assembly OS " + assemblyOS);
-  	System.out.println("CPU " + cpu);
-  	System.out.println("Windows " + windows);
+    System.out.println("core " + core);
+    System.out.println("RAM " + ram);
+    System.out.println("SSD " + ssd);
+    System.out.println("HDD " + hdd);
+    System.out.println("CPU Speed " + cpuSpeed);
+    System.out.println("assembly OS " + assemblyOS);
+    System.out.println("CPU " + cpu);
+    System.out.println("Windows " + windows);
   }
 }
