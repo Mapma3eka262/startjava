b45e2ed5d5bdbcc2434de4a19989534fcdac9db3
Mapma3eka262 <vlaskofanov@gmail.com>
2020-09-28 20:37:10 +0500

    Изменения согласно комментарию в Slack

--- Lesson_1/Game.java
+++ Lesson_1/Game.java
@@ -1,40 +1,40 @@
 public class Game {	
-    public static void main(String[] args) {
-	int comp = 36;
-	int firsttry = 50;
-	int secondtry = 25;
-	int thirdtry = 40;
-	int fourthtry = 36;
-	int count = 0;		
-	for (int i = 0; i < 4; i++) {
-		if (i == 0) {
-			count = firsttry;
-			System.out.println("Вы ввели число: " + count);
-		}
-		else {
-			if (i == 1) {
-				count = secondtry;
+  public static void main(String[] args) {
+		int comp = 36;
+		int firsttry = 50;
+		int secondtry = 25;
+		int thirdtry = 40;
+		int fourthtry = 36;
+		int count = 0;		
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) {
+				count = firsttry;
 				System.out.println("Вы ввели число: " + count);
 			}
 			else {
-				if (i == 2) {
-					count = thirdtry;
+				if (i == 1) {
+					count = secondtry;
 					System.out.println("Вы ввели число: " + count);
 				}
 				else {
-					count = fourthtry;
-					System.out.println("Вы ввели число: " + count);
+					if (i == 2) {
+						count = thirdtry;
+						System.out.println("Вы ввели число: " + count);
+					}
+					else {
+						count = fourthtry;
+						System.out.println("Вы ввели число: " + count);
+					}
 				}
 			}
+			if (count > comp)
+				System.out.println("Введенное вами число больше загаданного");
+			else {
+				if (count < comp)
+					System.out.println("Введенное вами число меньше загаданного");
+				else
+					System.out.println("Вы угадали, загаданным число было "+ comp);
+			}
 		}
-		if (count > comp)
-			System.out.println("Введенное вами число больше загаданного");
-		else {
-			if (count < comp)
-				System.out.println("Введенное вами число меньше загаданного");
-			else
-				System.out.println("Вы угадали, загаданным число было "+ comp);
-		}
-	}
 	}
 }
