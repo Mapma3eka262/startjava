b20715e14990c509c6ecdf0c8ee9c1e3c455747a
Mapma3eka262 <vlaskofanov@gmail.com>
2020-09-28 20:07:13 +0500

    Изменения согласно комментарию в Slack

--- Lesson_1/Unicode.java
+++ Lesson_1/Unicode.java
@@ -1,13 +1,10 @@
 public class Unicode {	
     public static void main(String[] args) {
-		char unicode = 0;
 		for (int i = 33; i <= 126; i++) {
 			if (i != 126) {
-				unicode = (char)(i);
-				System.out.print(unicode + ", ");
-			}
-			else 
-				System.out.println(unicode);
+				System.out.print((char)(i) + ", ");
+			} else 
+				System.out.println((char)(i));
 		}
 	}
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,20 +1,21 @@
 public class Variable {	
     public static void main(String[] args) {
-        byte core = 6;
-		short RAM = 12;
-		int SSD = 120;
-		long HDD = 1024;
-		float CPUSpeed = 3.8f;
+		byte core = 6;
+		short ram = 12;
+		int ssd = 120;
+		long hdd = 1024;
+		float cpuSpeed = 3.8f;
 		double assemblyOS = 19041.508;
-		char CPU = 'A';
-		boolean window = true;
+		char cpu = 'A';
+		boolean windows = true;
+		
 		System.out.println("core " + core);
-		System.out.println("RAM " + RAM);
-		System.out.println("SSD " + SSD);
-		System.out.println("HDD " + HDD);
-		System.out.println("CPU Speed " + CPUSpeed);
+		System.out.println("RAM " + ram);
+		System.out.println("SSD " + ssd);
+		System.out.println("HDD " + hdd);
+		System.out.println("CPU Speed " + cpuSpeed);
 		System.out.println("assembly OS " + assemblyOS);
-		System.out.println("CPU " + CPU);
-		System.out.println("window " + window);
+		System.out.println("CPU " + cpu);
+		System.out.println("Windows " + windows);
     }
 }
