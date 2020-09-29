39bb800a1d8105c0cebf538558902cba2936d523
Mapma3eka262 <vlaskofanov@gmail.com>
2020-09-29 16:20:29 +0500

    Внесение изменений

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,43 +1,40 @@
 public class Calculator {
     public static void main(String[] args) {
-        char operation = '%';  // Введите математическое знак
+        char operation = '%';  // Мат. знак
         int firstNum = 39;     // Введите 1 число для математических операций
         int secondNum = 7;     // Введите 2 число для математических операций
         double result = 0;
 
-        if (choose == '+') {
-            System.out.println("Выбран математический знак: " + choose);
+        if (operation == '+') {
+            System.out.println("Выбран математический знак: " + operation);
             result = firstNum + secondNum;
-        System.out.println("Результат сложения чисел " + firstNum + " и " + secondNum + ": " + result);
-        } else {
-            if (choose == '-') {
-                System.out.println("Выбран математический знак: " + choose);
-                result = firstNum - secondNum;
-                System.out.println("Результат вычитания чисел " + firstNum + " и " + secondNum + ": " + result);
-            } else if (choose == '*') {
-                System.out.println("Выбран математический знак: " + choose);
-                result = firstNum * secondNum;
-                System.out.println("Результат умножения чисел " + firstNum + " и " + secondNum + ": " + result);
-            } else if (choose == '/') {
-                System.out.println("Выбран математический знак: " + choose);
-                result = firstNum / secondNum;
-                System.out.println("Результат деления чисел " + firstNum + " и " + secondNum + ": " + result);
-            } else if (choose == '^') {
-                System.out.println("Выбран математический знак: " + choose);
-                for (int i = 0; i < 3; i++) {
-                    if (i == 0) {
-                        result=secondNum;
-                    }
-                    else {
-                        result *= secondNum;
-                    }
+            System.out.println("Результат сложения чисел " + firstNum + " и " + secondNum + ": " + result);
+        } else if (operation == '-') {
+            System.out.println("Выбран математический знак: " + operation);
+            result = firstNum - secondNum;
+            System.out.println("Результат вычитания чисел " + firstNum + " и " + secondNum + ": " + result);
+        } else if (operation == '*') {
+            System.out.println("Выбран математический знак: " + operation);
+            result = firstNum * secondNum;
+            System.out.println("Результат умножения чисел " + firstNum + " и " + secondNum + ": " + result);
+        } else if (operation == '/') {
+            System.out.println("Выбран математический знак: " + operation);
+            result = firstNum / secondNum;
+            System.out.println("Результат деления чисел " + firstNum + " и " + secondNum + ": " + result);
+        } else if (operation == '^') {
+            System.out.println("Выбран математический знак: " + operation);
+            for (int i = 0; i < secondNum; i++) {
+                if (i == 0) {
+                    result = firstNum;
+                } else {
+                    result *= firstNum;
                 }
-                System.out.println("Результат возведения числа " + secondNum + " в степень 3: " + result);
-            } else {
-                System.out.println("Выбран математический знак: " + choose);
-                result = firstNum % secondNum;
-                System.out.println("Остаток от деления чисел " + firstNum + " и " + secondNum + ": " + result);
             }
+            System.out.println("Результат возведения числа " + secondNum + " в степень 3: " + result);
+        } else {
+            System.out.println("Выбран математический знак: " + operation);
+            result = firstNum % secondNum;
+            System.out.println("Остаток от деления чисел " + firstNum + " и " + secondNum + ": " + result);
         }
     }
 }
--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -4,18 +4,21 @@ public class ConditionalStatement {
         if (age > 20) {
             System.out.println("Вы старше 20 лет");
         } 
+
         String sex = "Мужчина";
         if (sex.equals("Мужчина")){
             System.out.println("Вы мужского пола");
         } else {
             System.out.println("Вы не мужского пола");
         }
+
         double height = 1.85;
         if (height < 1.80){
             System.out.println("Вы ниже 1.80 метра");
         } else {
             System.out.println("Вы выше 1.80 метра");
         } 
+        
         char firstNameLetter = 'В';
         if (firstNameLetter == 'М') {
             System.out.println("Ваше имя начинается с буквы 'М'");
