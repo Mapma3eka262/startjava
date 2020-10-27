069e030bbcfcb053e36b8504ef9dbcf8679627dc
Mapma3eka262 <vlaskofanov@gmail.com>
2020-10-27 11:59:20 +0500

    Проверка чисел

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class GuessNumber {
     private int hiddenNum;
-    Random random = new Random();
-    Scanner scan = new Scanner(System.in);
-    Player playerOne;
-    Player playerTwo;
+    private Random random = new Random();
+    private Scanner scan = new Scanner(System.in);
+    private Player playerOne;
+    private Player playerTwo;
     
     public GuessNumber(Player playerOne, Player playerTwo) {
         this.playerOne = playerOne;
@@ -19,40 +19,33 @@ public class GuessNumber {
         while (true) {
             System.out.println("Игрок " + playerOne.getName() + " введите число");
             playerOne.setNumber(scan.nextInt());
-            if (playerOne.getNumber() == hiddenNum) {
+            if (playerOne.getNumber() != hiddenNum) {
+                if (playerOne.getNumber() > hiddenNum) {
+                    System.out.println("Число введенное игроком " + playerOne.getName() + " больше загаданного");
+                    System.out.println("Игрок " + playerTwo.getName() + " введите число");
+                    playerTwo.setNumber(scan.nextInt());
+                    scan.nextLine();
+                } else {
+                    System.out.println("Число введенное игроком " + playerOne.getName() + " меньше загаданного");
+                    System.out.println("Игрок " + playerTwo.getName() + " введите число");
+                    playerTwo.setNumber(scan.nextInt());
+                    scan.nextLine();
+                }
+            } else {
                 System.out.println("Игрок " + playerOne.getName() + " победил");   
                 break;
-            } else if (playerOne.getNumber() > hiddenNum){
-                System.out.println("Число введенное игроком " + playerOne.getName() + " больше загаданного");
-                System.out.println("Игрок " + playerTwo.getName() + " введите число");
-                playerTwo.setNumber(scan.nextInt());
-                scan.nextLine();
-                if (playerTwo.getNumber() == hiddenNum) {
-                    System.out.println("Игрок " + playerTwo.getName() + " победил");
-                    break;
-                } else if (playerTwo.getNumber() > hiddenNum){
+            }
+            
+            if (playerTwo.getNumber() != hiddenNum) {
+                if (playerTwo.getNumber() > hiddenNum) {
                     System.out.println("Число введенное игроком " + playerTwo.getName() + " больше загаданного");
-                } else if (playerTwo.getNumber() < hiddenNum){
+                } else {
                     System.out.println("Число введенное игроком " + playerTwo.getName() + " меньше загаданного");
                 }
-            } else if (playerOne.getNumber() < hiddenNum){
-                System.out.println("Число введенное игроком " + playerOne.getName() + " меньше загаданного");
-                System.out.println("Игрок " + playerTwo.getName() + " введите число");
-                playerTwo.setNumber(scan.nextInt());
-                scan.nextLine();
-                if (playerTwo.getNumber() == hiddenNum) {
-                    System.out.println("Игрок " + playerTwo.getName() + " победил");
-                    break;
-                } else if (playerTwo.getNumber() > hiddenNum){
-                    System.out.println("Число введенное игроком " + playerTwo.getName() + " больше загаданного");
-                } else if (playerTwo.getNumber() < hiddenNum){
-                    System.out.println("Число введенное игроком " + playerTwo.getName() + " меньше загаданного");
-                }
+            } else {
+                System.out.println("Игрок " + playerTwo.getName() + " победил");   
+                break;
             }
         }
     }
 }
-
-
-
-
